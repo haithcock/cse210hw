bbe449c14c2c3d3ecbf55309abb440d321f88b49
haithcock <dakota.haith1997@gmail.com>
2025-01-10 17:04:44 -0500

    newww

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("Enter number: ");
+        string valueFromUser = Console.ReadLine();
+
+
+
+        double 数 = double.Parse(valueFromUser);
+        string 字母 = "";
+
+        if (数 >= 90 && 数 <= 100)
+        {
+            字母 = "A";
+        }
+        else if (数 >= 80 && 数 <= 89.9999999999999999999)
+        {
+            字母 = "B";
+        }
+        else if (数 >= 70 && 数 <= 79.999999999999999999999)
+        {
+            字母 = "C";
+        }
+        else if (数 >= 60 && 数 <= 69.99999999999999999999)
+        {
+            字母 = "D";
+        }
+        else if (数 >= 0 && 数 <= 59.9999999999999999999999999)
+        {
+            字母 = "F";
+        }
+        else
+        {
+            Console.WriteLine("Invalid Grade.");
+            return;
+        }
+        if (数 >= 70)
+        {
+            Console.WriteLine($"Congragulations you passed the class! with a {字母}");
+        }
+        else
+        {
+            Console.WriteLine($"You did not pass. You failed with a {字母} TRY HARDER NEXT TIME!");
+        }
     }
 }
