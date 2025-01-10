03e2b9bf07a79c95a504ee513a3f12ab1ca5026f
haithcock <dakota.haith1997@gmail.com>
2025-01-10 18:09:41 -0500

    sure

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -5,25 +5,5 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
-
-        Random randomGenerator = new Random();
-        int number = randomGenerator.Next(1, 11);
-
-        Console.WriteLine("What number is your guess?");
-        string userGuess = Console.ReadLine();
-        int guess = int.Parse(userGuess);
-    
-        if (guess < number)
-        {
-            Console.WriteLine("Higher");
-        }
-        else if (guess > number)
-        {
-            Console.WriteLine("Lower");
-        }
-        else
-        {
-            Console.WriteLine("You guessed right!");
-        }
     }
 }
