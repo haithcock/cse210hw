5adbd4e23251a0c17c7924aebf265191122014f2
haithcock <dakota.haith1997@gmail.com>
2025-01-11 15:55:15 -0500

    working on exercise 5 added a few functions

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -2,8 +2,43 @@ using System;
 
 class Program
 {
+    static string username;
+    static double favoritenumber;
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        DisplayMessage();
+        UserFavoriteNumber();
+        last();
+
+    }
+    static void DisplayMessage()
+    {
+        Console.WriteLine("What is your name?");
+        username = Console.ReadLine();
+        Console.WriteLine($"Hello, {username}");
+    }
+    static void UserFavoriteNumber()
+    {
+        Console.WriteLine("What is your favorite Number?");
+        string input = Console.ReadLine();
+
+        if (double.TryParse(input, out favoritenumber))
+        {
+            Console.WriteLine($"Your favorite number is {favoritenumber}");
+        }
+        else
+        {
+            Console.WriteLine("Invalid number. Please enter a valid number with digits and/or decimals.");
+        }
+    }
+    static void third()
+    {
+
+
+    }
+    static void last()
+    {
+        Console.WriteLine($"Your name is {username} and your favorite number is {favoritenumber}");
     }
 }
