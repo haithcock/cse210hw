e775398cc80534d64989f20a17ae3a88edf3e371
haithcock <dakota.haith1997@gmail.com>
2025-01-11 12:26:57 -0500

    updated exercise 4

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,43 +1,82 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise4 Project.");
-        List<string> shins = new List<string>();
-        shins.Add("Seongyong");
-        shins.Add("Laurie");
-        Console.WriteLine(shins.Count);
-        Console.WriteLine($"Once upon a time there were {shins.Count} people");
 
-        foreach (string familymembow in shins)
+        Console.Write("  Enter number: ");
+        string numbersarejustwords = Console.ReadLine();
+        int numbersarenownumbers = int.Parse(numbersarejustwords);
+        List<int> usermadelist = new List<int>();
+        usermadelist.Add(numbersarenownumbers);
+        int sum = 0;
+
+        while (numbersarenownumbers != 0)
         {
-            Console.WriteLine(familymembow);
-        }
+            Console.WriteLine("Enter a number");
 
-        shins.Add("Adam");
-        shins.Add("Emma");
-        shins.Add("Caleb");
-        shins.Add("Jared");
-        shins.Add("Laura");
+            numbersarejustwords = Console.ReadLine();
+            numbersarenownumbers = int.Parse(numbersarejustwords);
 
-        Console.WriteLine($"But then they got busy and soon their family became a family of {shins.Count}.");
-
-        foreach (string familymembow in shins)
-        {
-            string familymembow2 = familymembow;
-            if (familymembow2 = "Emma")
+            if (numbersarenownumbers == 0)
             {
-                Console.WriteLine($"{familymembow} was nice this year.");
-            }
 
+            }
             else
             {
-                Console.WriteLine($"{familymembow} was naughty this year.");
+
+                usermadelist.Add(numbersarenownumbers);
+
+
             }
+            sum += numbersarenownumbers;
 
         }
+        float average = ((float)sum / usermadelist.Count);
+        Console.WriteLine($"Your average is {average}");
+        Console.WriteLine($"The sum of the numbers you added is {sum}");
+
+
+
+
+
+
+        //        while (guess != 0);
+        //   List<string> shins = new List<string>();
+        //   shins.Add("Seongyong");
+        //   shins.Add("Laurie");
+        //  Console.WriteLine(shins.Count);
+        //   Console.WriteLine($"Once upon a time there were {shins.Count} people");
+
+        //        foreach (string familymembow in shins)
+        //        {
+        //            Console.WriteLine(familymembow);
+        //       }
+        //
+        //       shins.Add("Adam");
+        //       shins.Add("Emma");
+        //       shins.Add("Caleb");
+        //       shins.Add("Jared");
+        //       shins.Add("Laura");
+        //
+        //        Console.WriteLine($"But then they got busy and soon their family became a family of {shins.Count}.");
+        //
+        //        foreach (string familymembow in shins)
+        //        {
+        //            if (familymembow == "Emma")
+        //            {
+        //               Console.WriteLine($"{familymembow} was nice this year.");
+        //            }
+
+        //            else
+        //            {
+        //               Console.WriteLine($"{familymembow} was naughty this year.");
+        //           }
+        //
+        //      }
 
     }
 }
