701d2d44b99f7c0ef9cc5ea8fb40582214146eea
haithcock <dakota.haith1997@gmail.com>
2025-01-11 12:40:55 -0500

    added usermadelist.Min/Max();

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -6,14 +6,17 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise4 Project.");
-
-        Console.Write("  Enter number: ");
+        int sum = 0;
+        Console.Write("  Enter a number (Press 0 to quit and compute your sum, average, and ): ");
         string numbersarejustwords = Console.ReadLine();
         int numbersarenownumbers = int.Parse(numbersarejustwords);
+        sum += numbersarenownumbers;
         List<int> usermadelist = new List<int>();
         usermadelist.Add(numbersarenownumbers);
-        int sum = 0;
 
+        int highestnumber = usermadelist.Max();
+        int lowestnumber = usermadelist.Min();
+        
         while (numbersarenownumbers != 0)
         {
             Console.WriteLine("Enter a number");
@@ -38,6 +41,8 @@ class Program
         float average = ((float)sum / usermadelist.Count);
         Console.WriteLine($"Your average is {average}");
         Console.WriteLine($"The sum of the numbers you added is {sum}");
+        Console.WriteLine($"Your highest number is {highestnumber}:);
+        Console.WriteLine($"Your lowest number is {lowerstnumber}");
 
 
 
