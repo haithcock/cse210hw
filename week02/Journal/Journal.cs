a06ac0c092031dcd12093060b0cba0b163048e68
haithcock <dakota.haith1997@gmail.com>
2025-01-17 15:30:08 -0500

    updates

--- week02/Journal/Journal.cs
+++ week02/Journal/Journal.cs
@@ -1,8 +1,8 @@
-using System
-using System.Collections.Generic
-using System.Linq
-using System.Text
-using System.Threading.Tasks
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace JournalApp
 {
@@ -10,7 +10,7 @@ namespace JournalApp
     {
         public void Run()
         {
-            
+
         }
         private void CreateJournalFile() { }
  
