3aa56d8eb75506fdcebeac60e5e7821c12ae9dc6
haithcock <dakota.haith1997@gmail.com>
2025-01-17 15:10:26 -0500

    updates

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -2,20 +2,17 @@ using System;
 using System.IO;
 using System.Text;
 
-
 namespace JournalApp
 {
-    
     class Program
     {
         static void Main(string[] args)
         {
-                Journal myJournal = new Journal(); 
-                myJournal.Run();
+            Journal myJournal = new Journal();
+            myJournal.Run();
         }
     }
 }
-
 
 /* public static void WriteAllText (string path, string? contents); 
 - this creates a new file,
