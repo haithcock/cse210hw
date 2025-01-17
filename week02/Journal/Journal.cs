1b0bc53830898823ecc37b1d6f4f44918c7b4321
haithcock <dakota.haith1997@gmail.com>
2025-01-17 15:56:29 -0500

    added ascii art and color changes for console.

--- week02/Journal/Journal.cs
+++ week02/Journal/Journal.cs
@@ -9,23 +9,55 @@ namespace JournalApp
 {
     public class Journal
     {
+        private string TitleArt = @"⣿⣿⣿⣿⣿⠟⠋⠄⠄⠄⠄⠄⠄⠄⢁⠈⢻⢿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⠃⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠈⡀⠭⢿⣿⣿⣿⣿
+⣿⣿⣿⣿⡟⠄⢀⣾⣿⣿⣿⣷⣶⣿⣷⣶⣶⡆⠄⠄⠄⣿⣿⣿⣿
+⣿⣿⣿⣿⡇⢀⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⠄⠄⢸⣿⣿⣿⣿
+⣿⣿⣿⣿⣇⣼⣿⣿⠿⠶⠙⣿⡟⠡⣴⣿⣽⣿⣧⠄⢸⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣾⣿⣿⣟⣭⣾⣿⣷⣶⣶⣴⣶⣿⣿⢄⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⡟⣩⣿⣿⣿⡏⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣹⡋⠘⠷⣦⣀⣠⡶⠁⠈⠁⠄⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣍⠃⣴⣶⡔⠒⠄⣠⢀⠄⠄⠄⡨⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣦⡘⠿⣷⣿⠿⠟⠃⠄⠄⣠⡇⠈⠻⣿⣿⣿⣿
+⣿⣿⣿⣿⡿⠟⠋⢁⣷⣠⠄⠄⠄⠄⣀⣠⣾⡟⠄⠄⠄⠄⠉⠙⠻
+⡿⠟⠋⠁⠄⠄⠄⢸⣿⣿⡯⢓⣴⣾⣿⣿⡟⠄⠄⠄⠄⠄⠄⠄⠄
+⠄⠄⠄⠄⠄⠄⠄⣿⡟⣷⠄⠹⣿⣿⣿⡿⠁⠄⠄⠄⠄⠄⠄⠄⠄
+⠄⠄⠄⠄⠄⠄⣸⣿⡷⡇⠄⣴⣾⣿⣿⠃⠄⠄⠄⠄⠄⠄⠄⠄⠄
+⠄⠄⠄⠄⠄⠄⣿⣿⠃⣦⣄⣿⣿⣿⠇⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+⠄⠄⠄⠄⠄⢸⣿⠗⢈⡶⣷⣿⣿⡏⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄";
         public void Run()
         {
+            Title = "Journal App";
             DisplayIntro();
             // To do: Rest of the program
             DisplayOutro();
         }
-        private void CreateJournalFile() { }
+
+        private void CreateJournalFile() //Checks to see if file exists then creates one if it doesn't.
+        {
+
+        }
 
         private void DisplayIntro()
         {
-            WriteLine("Thanks for using the journal!");
-            ReadKey(true);
+            ForegroundColor = ConsoleColor.Green;
+            BackgroundColor = ConsoleColor.Magenta;
+            Clear();
+            WriteLine(TitleArt);
+            WriteLine(" \n  Greetings Comrade,\n\n  Welcome to the best Journal App you'll ever grade.");
+            WaitForKey();
         }
 
         private void DisplayOutro()
         {
-            WriteLine("Journal App.");
+            WriteLine("\nArt from: https://www.twitchquotes.com/copypastas/3553");
+            WriteLine("     谢谢 Comrade!\n Thank you for good grade! \n 500+ Social Credit Points to you ");
+            ReadKey(true);
+        }
+        private void WaitForKey()
+        {
+            ForegroundColor = ConsoleColor.DarkGray;
+            WriteLine("\nPress any key...");
             ReadKey(true);
         }
 
