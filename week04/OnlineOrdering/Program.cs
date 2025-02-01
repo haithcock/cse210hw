d5053ac231f1ea7da51446904fba33b472f6a391
haithcock <dakota.haith1997@gmail.com>
2025-02-01 16:40:34 -0500

    finished

--- week04/OnlineOrdering/Program.cs
+++ week04/OnlineOrdering/Program.cs
@@ -11,26 +11,26 @@ class Program
         Address address2 = new Address("574 Bryan Dr", "Apex", "NC", "27502", "United States");
 
         // Create customer instances
-        Customer customer1 = new Customer(1, "John Doe", "john.doe@example.com", "555-1234", address1);
-        Customer customer2 = new Customer(2, "Jane Smith", "jane.smith@example.com", "555-5678", address2);
+        Customer customer1 = new Customer(1, "신소라", "doodlest.bug@navercorp.com", "010-1234-8765", address1);
+        Customer customer2 = new Customer(2, "Daniel Boone", "daniel.boone@gmail.com", "555-5678", address2);
 
         // Create product instances
-        Product product1 = new Product("Apple", 0.75, 150, 101);
-        Product product2 = new Product("Milk", 2.85, 50, 102);
-        Product product3 = new Product("Bread", 1.50, 40, 103);
-        Product product4 = new Product("Butter", 3.50, 30, 104);
-        Product product5 = new Product("Cheese", 4.50, 25, 105);
+        Product product1 = new Product("Detent Spring", 0.75, 20, 101);
+        Product product2 = new Product("Detent", 2.85, 20, 102);
+        Product product3 = new Product("20-inch Barrel", 150, 5, 103);
+        Product product4 = new Product("Butter", 3.50, 2, 104);
+        Product product5 = new Product("Hi-Chews", 4.50, 25, 105);
 
         // Create order instances
         Order order1 = new Order(customer1);
         Order order2 = new Order(customer2);
 
         // Add products to orders
-        order1.AddProduct(product1);
-        order1.AddProduct(product2);
+        order1.AddProduct(product4);
+        order1.AddProduct(product5);
         order2.AddProduct(product3);
-        order2.AddProduct(product4);
-        order2.AddProduct(product5);
+        order2.AddProduct(product1);
+        order2.AddProduct(product2);
 
         // Display order details
         DisplayOrderDetails(order1);
