060a10f2554c52ff390e7e40fa9d146d5dfd9f94
haithcock <dakota.haith1997@gmail.com>
2025-02-01 16:05:44 -0500

    created product class

---
+++ week04/OnlineOrdering/Product.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+
+
+public class Product
+{
+    public string Name { get; set; }
+    public double Price { get; set; }
+    public int Quantity { get; set; }
+
+    public int ProductID {get; set; }
+
+    public Product(string name, double price, int quantity, int productId)
+    {
+        ProductID = productId;
+        Name = name;
+        Price = price;
+        Quantity = quantity;
+    }
+}
