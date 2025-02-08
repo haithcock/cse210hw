fc0687a90b65e3bc1301dab7f0fc4de6c60f09a3
haithcock <dakota.haith1997@gmail.com>
2025-02-08 16:01:29 -0500

    class notes

---
+++ week06/justnotes.cs
@@ -0,0 +1,25 @@
+/*
+
+
+Questions im asking myself and writing
+ out my thoughts in order to absorb
+ more information. 
+
+What am I reading right now? 
+
+I think its something about inheritence
+between a parent class and a child class.
+
+Actually,
+  I am reading about method overriding. 
+Specifically about the ability of a child
+class to over write a method it inherited 
+from its parent class. 
+
+
+In this way the method's name stays 
+the same but the behavior or the way
+ it runs its code changes.
+
+
+*/
