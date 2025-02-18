6d6985dce023f2aeb745b02d4023d76fff784a78
haithcock <dakota.haith1997@gmail.com>
2025-02-18 10:42:37 -0500

    changing Activity.cs

--- week07/ExerciseTracking/Activity.cs
+++ week07/ExerciseTracking/Activity.cs
@@ -12,17 +12,14 @@ public abstract class Activity
         _lengthInMinutes = length;
     }
 
-    public string Date => _date;
-    public int Length => _lengthInMinutes;
-
     public abstract double GetDistance();
     public abstract double GetSpeed();
     public abstract double GetPace();
 
 public virtual string GetSummary()
 {
-    return $"{Date} {GetType().Name}\n" +
-           $"Duration: {Length} minutes\n" +
+    return $"{_date} {GetType().Name}\n" +
+           $"Duration: {_lengthInMinutes} minutes\n" +
            $"Distance: {GetDistance():F1} miles\n" +
            $"Speed: {GetSpeed():F1} mph\n" +
            $"Pace: {GetPace():F1} min per mile\n";
